b7fd16689a4e847212727f3a781a782a2521c99e
FirdaousOch <148466842+FirdaousOch@users.noreply.github.com>
2024-06-19 13:49:06 +0100

    Create questions2.js

---
+++ questions2.js
@@ -0,0 +1,82 @@
+let MCQS = [
+    {
+        "question": "Quelle certification importante Obiose Ingénierie a-t-elle obtenue pour l'année 2020?",
+        "choice1": "ISO 9001",
+        "choice2": "ISO 45001",
+        "choice3": "ISO 14001",
+        "choice4": "Certificat d'économie d'énergie (CEE)",
+        "answer": 3
+    },
+    {
+        "question": "Quel logiciel de diagnostic énergétique a été utilisé pour le diagnostic à Amayé-sur-Orne?",
+        "choice1": "AutoCAD",
+        "choice2": "BAO EVO2",
+        "choice3": "Sketchup",
+        "choice4": "DIALux",
+        "answer": 1
+    },
+    {
+        "question": "Quel est le principal défi rencontré par Obiose Ingénierie lors de la gestion des appels d’offres?",
+        "choice1": "Manque de logiciels adaptés",
+        "choice2": "Difficulté à obtenir des partenariats",
+        "choice3": "Compétences insuffisantes",
+        "choice4": "Choisir rapidement les appels d’offres à poursuivre",
+        "answer": 3
+    },
+    {
+        "question": "Quelle méthode de calcul est utilisée pour l'audit énergétique selon le document?",
+        "choice1": "Méthode des coefficients binomiaux",
+        "choice2": "Méthode de calcul des coûts globalisés",
+        "choice3": "Méthode Th-CE ex",
+        "choice4": "Méthode de simulation numérique",
+        "answer": 2
+    },
+    {
+        "question": "Quel type de chaudière Obiose Ingénierie propose-t-elle pour l’amélioration énergétique?",
+        "choice1": "Chaudière à gaz naturel",
+        "choice2": "Chaudière biomasse",
+        "choice3": "Chaudière électrique",
+        "choice4": "Chaudière double service",
+        "answer": 3
+    },
+    {
+        "question": "Dans quel contexte Obiose Ingénierie utilise-t-elle le logiciel Plancal Nova?",
+        "choice1": "Pour la facturation des projets",
+        "choice2": "Pour le dessin de plans architecturaux",
+        "choice3": "Pour la gestion de la relation client",
+        "choice4": "Pour le calcul de performance énergétique",
+        "answer": 1
+    },
+    {
+        "question": "Quel dispositif utilise Obiose Ingénierie pour le relevé de température?",
+        "choice1": "Thermomètre infrarouge",
+        "choice2": "Caméra thermique",
+        "choice3": "Capteurs de température IoT",
+        "choice4": "Balances thermiques programmables",
+        "answer": 0
+    },
+    {
+        "question": "Quelle est la stratégie principale d'Obiose Ingénierie face aux appels d’offres?",
+        "choice1": "Participation sélective basée sur le coût prévisionnel",
+        "choice2": "Réponse à tous les appels sans sélection",
+        "choice3": "Priorisation basée sur la facilité de réalisation",
+        "choice4": "Analyse approfondie avant décision",
+        "answer": 0
+    },
+    {
+        "question": "Quelle caractéristique des fenêtres est importante dans les études énergétiques réalisées par Obiose Ingénierie?",
+        "choice1": "La couleur et le design",
+        "choice2": "Le coefficient de transmission thermique",
+        "choice3": "La taille et le nombre de battants",
+        "choice4": "Le type de matériau du cadre",
+        "answer": 1
+    },
+    {
+        "question": "Quel critère Obiose Ingénierie utilise-t-il pour la sélection des collaborateurs sur un projet?",
+        "choice1": "Ancienneté dans l'entreprise",
+        "choice2": "Compétence et disponibilité",
+        "choice3": "Proximité géographique au projet",
+        "choice4": "Taille de l'entreprise partenaire",
+        "answer": 2
+    }
+]
